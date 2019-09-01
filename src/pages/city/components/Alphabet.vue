a6979fd0706571c0319d8ddc2c8a751ca24a3df5
kai <2292572541@qq.com>
2019-09-01 22:23:53 +0800

    perfomance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,11 @@
 <template>
     <ul class="list">
-        <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+        <li class="item" v-for="item of letters" :key="item" :ref="item"
+          @click="handleLetterClick"
+          @touchstart="handleTouchStart"
+          @touchmove="handleTouchMove"
+          @touchend="handleTouchEnd"
+        >{{item}}</li>
     </ul>
 </template>
 
@@ -9,6 +14,57 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      // 标识位
+      touchStatus: false,
+      startY: 0,
+      // 节流
+      titmer: null
+    }
+  },
+  updated () {
+    // 页面完成渲染了以后执行
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    // 兄弟传值  传值数据到 City.vue ，然后 City 接收数据 再传值到 List.vue
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      // 手指触摸的时候
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      // 手指上下滑动的时候
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(()  => {
+          const touchY = e.touches[0].clientY - 72
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        },16)
+      }
+    },
+    handleTouchEnd () {
+      // 手指停止滑动
+      this.touchStatus = false
+    }
   }
 }
 </script>
@@ -26,6 +82,6 @@ export default {
   bottom 0
   width 2rem
   .item
-    line-height 1.7rem
+    line-height 1.8rem
     text-align center
 </style>
