c964d9233ed627dc5bbff36ad2d8b92977b95724
kai <2292572541@qq.com>
2019-09-03 16:35:36 +0800

    header animation

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+            <div class="iconfont header-back">&#xe601;</div>
+        </router-link>
+        <router-link tag="div" to="/" class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+            <div class="iconfont header-fixed-back">&#xe601;</div>
+            景点详情
+        </router-link>
+        <div class="content"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+.content
+  height 60rem
+.header-abs
+  position absolute
+  left 1rem
+  top .6rem
+  width 2.8rem
+  height 2.8rem
+  line-height 2.8rem
+  border-radius 5rem
+  text-align center
+  background rgba(0,0,0,.8)
+  .header-back
+    color #fff
+    font-size 1.5rem
+.header-fixed
+  position fixed
+  top 0
+  left 0
+  right 0
+  height $harderHeight
+  line-height $harderHeight
+  text-align center
+  color #ffffff
+  background $bgColor
+  font-size 1.3rem
+  .header-fixed-back
+    position absolute
+    left 1rem
+    top .6rem
+    width 2.8rem
+    height 2.8rem
+    line-height 2.8rem
+    border-radius 5rem
+    text-align center
+    font-size 1.5rem
+</style>
